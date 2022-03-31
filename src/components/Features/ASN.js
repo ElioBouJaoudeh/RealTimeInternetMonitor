32f4a6fe246697ca0c7f79285a9d7050fe4be4d8
LeaHarrouz <lea.harrouz@net.usj.edu.lb>
2022-03-31 13:59:47 +0300

    Combining the 2 python files

--- src/components/Features/ASN.js
+++ src/components/Features/ASN.js
@@ -1,10 +1,10 @@
-import React,{useState,useEffect} from "react";
-import './Visibility.css';
-import ProgressBar from './ProgressBar';
+import React, { useState, useEffect } from "react";
+import "./Visibility.css";
+import ProgressBar from "./ProgressBar";
 import { IconContext } from "react-icons/lib";
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Button } from "../../globalStyles";
-import { Container } from '../../globalStyles';
+import { Container } from "../../globalStyles";
 
 export const VisContainer = styled(Container)`
   display: flex;
@@ -16,11 +16,11 @@ export const VisContainer = styled(Container)`
 
 export const ButtonVis = styled.button`
   border-radius: 4px;
-  background: ${({ primary }) => (primary ? '#07080a' : '##9aa5b3')};
+  background: ${({ primary }) => (primary ? "#07080a" : "##9aa5b3")};
   white-space: nowrap;
-  padding: ${({ big }) => (big ? '12px 64px' : '10px 20px')};
+  padding: ${({ big }) => (big ? "12px 64px" : "10px 20px")};
   color: #fff;
-  font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+  font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
   outline: none;
   border: none;
   cursor: pointer;
@@ -28,44 +28,41 @@ export const ButtonVis = styled.button`
   &:hover {
     transition: all 0.3s ease-out;
     background: #fff;
-    background-color: ${({ primary }) => (primary ? '#9aa5b3' : '#9aa5b3')};
+    background-color: ${({ primary }) => (primary ? "#9aa5b3" : "#9aa5b3")};
   }
-
 `;
 
 function ASN() {
-  const [dataa,setData]=useState([{}])
-  useEffect(()=>{
-    fetch("/as").then(
-      res=>res.json()
-    ).then(
-      dataa => {
-        setData(dataa)
-        console.log(dataa["42020"]["List of prefixes"])
-      }
-    )
-  },[])
+  const [dataa, setData] = useState([{}]);
+  useEffect(() => {
+    fetch("/as")
+      .then((res) => res.json())
+      .then((dataa) => {
+        setData(dataa);
+        console.log(dataa["42020"]["List of prefixes"]);
+      });
+  }, []);
 
   const [progress, setProgress] = useState(0);
-  const [color, setColor] = useState('');
-  const colorArray = ['#fa0000', "#fa6900", "#7bff00", "#d9edfe"];
+  const [color, setColor] = useState("");
+  const colorArray = ["#fa0000", "#fa6900", "#7bff00", "#d9edfe"];
 
   const randomColor = () => {
-    if (dataa["42020"]["ipv4"]<50){
+    if (dataa["42020"]["ipv4"] < 50) {
       return colorArray[0];
-    }
-    else if(dataa["42020"]["ipv4"]>=50 && dataa["42020"]["ipv4"]<100){
+    } else if (dataa["42020"]["ipv4"] >= 50 && dataa["42020"]["ipv4"] < 100) {
       return colorArray[1];
-    }else if(dataa["42020"]["ipv4"]==100){
+    } else if (dataa["42020"]["ipv4"] == 100) {
       return colorArray[2];
-    }else{
+    } else {
       return colorArray[3];
     }
-  }
+  };
 
-  function getValue(){
-    if(typeof dataa.dictionary === 'undefined') {return 0;}
-    else{
+  function getValue() {
+    if (typeof dataa.dictionary === "undefined") {
+      return 0;
+    } else {
       return dataa["42020"]["ipv4"];
     }
   }
@@ -75,9 +72,9 @@ function ASN() {
     setProgress(progressValue);
     const randomProgressColor = randomColor();
     setColor(randomProgressColor);
-  }
+  };
 
-  const onChange = e => {
+  const onChange = (e) => {
     if (e.target.value) {
       progress = e.target.value;
       setProgress(progress);
@@ -86,33 +83,36 @@ function ASN() {
     } else {
       setProgress(0);
     }
-  }
+  };
 
   return (
     <VisContainer>
-    <div className="visibility-container">
-      <video src='/videos/blue.mp4' autoPlay loop muted />
-      <h1>General information about your connection:</h1>
-      {(typeof dataa.dictionary === 'undefined') ? (
-      <p>Loading...</p>
-    ):(
-        Object.keys(dataa["42020"]["List of prefixes"]).map((key, index) => (
-          <p key={index}> {key} : {dataa["42020"]["List of prefixes"][key]}</p> 
-        ))
-    )}
-    <div className="button-container">
-    <Button primary onClick={randomProgressValue}>
-          Check Visibility
-        </Button>
-    </div>
-    <ProgressBar 
+      <div className="visibility-container">
+        <video src="/videos/blue.mp4" autoPlay loop muted />
+        <h1>General information about your connection:</h1>
+        {typeof dataa.dictionary === "undefined" ? (
+          <p>Loading...</p>
+        ) : (
+          Object.keys(dataa["42020"]["List of prefixes"]).map((key, index) => (
+            <p key={index}>
+              {" "}
+              {key} : {dataa["42020"]["List of prefixes"][key]}
+            </p>
+          ))
+        )}
+        <div className="button-container">
+          <Button primary onClick={randomProgressValue}>
+            Check Visibility
+          </Button>
+        </div>
+        <ProgressBar
           progress={progress}
           size={250}
           strokeWidth={15}
-          circleOneStroke='#d9edfe'
+          circleOneStroke="#d9edfe"
           circleTwoStroke={color}
         />
-    </div>
+      </div>
     </VisContainer>
   );
 }
