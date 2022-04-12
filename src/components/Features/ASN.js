75c6e380d8dd5342f7abbdffb018df23046dc562
Elio <elionaoum.boujaoudeh@gmail.com>
2022-04-12 16:14:16 +0300

    Trying something

--- src/components/Features/ASN.js
+++ src/components/Features/ASN.js
@@ -84,7 +84,7 @@ function ASN() {
   const [val, setArray] = useState([]);
   const [keystaken, setKeys] = useState([]);
   useEffect(() => {
-    fetch("/as")
+    fetch("http://localhost:5000/as")
       .then((res) => res.json())
       .then((dataa) => {
         for (const key of Object.keys(dataa["42020"]["List of prefixes"])){
